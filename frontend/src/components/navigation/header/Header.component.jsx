b835a43edfb651f0a6c53a9beb8f5322619b997f
Ola Oyo <ola.oyo@gmail.com>
2023-02-12 11:40:34 +0100

    Header, footer, react router & styled components

---
+++ frontend/src/components/navigation/header/Header.component.jsx
@@ -0,0 +1,41 @@
+import { Outlet } from "react-router-dom";
+import {
+  HeaderStyles,
+  Logo,
+  Menu,
+  MenuItem,
+  Cart,
+  ProfileOuter,
+  ProfileInner,
+} from "./Header.styles";
+import Search from "../../inputs/search/Search.component";
+import { LoginButton } from "../../buttons/Buttons";
+
+function Header() {
+  return (
+    <>
+      <HeaderStyles>
+        <Logo>Meubel House</Logo>
+        <Search />
+        <Menu>
+          <MenuItem>Products</MenuItem>
+          <MenuItem>Admin</MenuItem>
+          <MenuItem>Profile</MenuItem>
+        </Menu>
+        <Cart>
+          <span className="material-symbols-outlined">shopping_cart</span>
+        </Cart>
+        <ProfileOuter>
+          <ProfileInner>
+            <p>OO</p>
+          </ProfileInner>
+        </ProfileOuter>
+        <LoginButton>Login</LoginButton>
+      </HeaderStyles>
+
+      <Outlet />
+    </>
+  );
+}
+
+export default Header;
