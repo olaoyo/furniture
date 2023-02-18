7b695d4469fc513ea78a86d8e1d131d49e2ea79b
Ola Oyo <ola.oyo@gmail.com>
2023-02-18 20:17:44 +0100

    Shop route

--- frontend/src/components/navigation/header/Header.component.jsx
+++ frontend/src/components/navigation/header/Header.component.jsx
@@ -1,6 +1,7 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import {
   HeaderStyles,
+  LogoGrid,
   Logo,
   Menu,
   MenuItem,
@@ -10,26 +11,38 @@ import {
 } from "./Header.styles";
 import Search from "../../inputs/search/Search.component";
 import { LoginButton } from "../../buttons/Buttons";
+import MeubelHouseLogo from "./img/MeubelHouseLogo.png";
 
 function Header() {
   return (
     <>
       <HeaderStyles>
-        <Logo>Meubel House</Logo>
+        <LogoGrid>
+          <Link to="/">
+            <Logo src={MeubelHouseLogo} />
+          </Link>
+        </LogoGrid>
+
         <Search />
+
         <Menu>
-          <MenuItem>Products</MenuItem>
+          <Link to="/shop">
+            <MenuItem>Shop</MenuItem>
+          </Link>
           <MenuItem>Admin</MenuItem>
           <MenuItem>Profile</MenuItem>
         </Menu>
+
         <Cart>
           <span className="material-symbols-outlined">shopping_cart</span>
         </Cart>
+
         <ProfileOuter>
           <ProfileInner>
             <p>OO</p>
           </ProfileInner>
         </ProfileOuter>
+
         <LoginButton>Login</LoginButton>
       </HeaderStyles>
 
