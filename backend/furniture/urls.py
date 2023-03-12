a4ab2af31e4329227ab59c438fbd30d50e70ff46
Ola Oyo <ola.oyo@gmail.com>
2023-03-12 17:04:50 +0100

    JWT customization

--- backend/furniture/urls.py
+++ backend/furniture/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 
 urlpatterns = [
-    path("users/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("users/login/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("", views.get_routes),
     path("furniture/", views.get_furnitures),
     path("furniture/<str:furniture_id>/", views.get_furniture),
--- backend/furniture/views.py
+++ backend/furniture/views.py
@@ -7,6 +7,27 @@ from rest_framework.response import Response
 from . models import Furniture
 from . serializers import FurnitureSerialzer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+     def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data["username"] = self.user.username
+        data["first_name"] = self.user.first_name
+        data["last_name"] = self.user.last_name
+        data["email"] = self.user.email
+
+        
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 def get_routes(request):
     return JsonResponse("Hello", safe=False)
 
