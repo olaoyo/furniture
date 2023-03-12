29090616747275a8b3682b9f61f6531c3c3ebd55
Ola Oyo <ola.oyo@gmail.com>
2023-03-12 13:54:11 +0100

    JSON web token installation & setup

--- backend/furniture/urls.py
+++ backend/furniture/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 
 urlpatterns = [
+    path("users/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("", views.get_routes),
     path("furniture/", views.get_furnitures),
     path("furniture/<str:furniture_id>/", views.get_furniture),
