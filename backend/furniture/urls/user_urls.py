d40bd48e578d1fbed06f4d46fc1089f0d510947c
Ola Oyo <ola.oyo@gmail.com>
2023-03-21 15:37:15 +0100

    Update profile endpoint

--- backend/furniture/urls/user_urls.py
+++ backend/furniture/urls/user_urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login/", views.MyTokenObtainPairView.as_view()),
     path("register/", views.register_user),
     path("profile/", views.get_user_profile),
+    path("profile/update/", views.update_user_profile),
 ]
